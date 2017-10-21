5218ea83c890eaaaa5ce8a5784adad9b705390d8
bittlingmayer <adam+code@signaln.com>
2017-10-21 11:33:32 +0400

    Add domain.js, will remove URLs from censormap/web

---
+++ domains.js
@@ -0,0 +1,40 @@
+const DOMAINS = [
+  // Information and education
+  "wikipedia.org", /*"wikimedia.org",*/ "wikibooks.org", "wikimediafoundation.org",
+  // Leaks
+  /*"wikileaks.org",*/ "liveleak.com",
+  // Search engines
+  "google.com", "yandex.com", "www.baidu.com", "bing.com",
+  // News and media
+  /*"dw.com", "reuters.com"*/ "bbc.com",
+  // Communication
+  "line.me", "telegram.org", "telegra.ph", "whatsapp.com", /*"viber.com",*/
+  // Social networks
+  "reddit.com", "twitter.com", "facebook.com", "linkedin.com", "stackexchange.com",
+  // Video and audio
+  "youtube.com", /*"dailymotion.com"*/, "soundcloud.com", "vimeo.com",
+  // Journalism
+  "cpj.org", "indexoncensorship.org", /*"rsf.org",*/
+  // Academic research
+  "sci-hub.cc", "arxiv.org",
+  // Tor
+  "torproject.org",
+  // Marketplaces
+  "alibaba.com", "amazon.com", "ebay.com", "rakuten.co.jp",
+  // Filesharing
+  /*"dropbox.com", "thepiratebay.se",*/ "scribd.com",
+  // Adult
+    // TODO
+  // Source code
+  "apache.org", "github.com", "gitlab.com", "bitbucket.com",
+  // TODO: Torrent sites, .onion
+  // Web archives
+  "archive.org"
+];
+// TODO: Country-specific TLDs
+
+// TODO:
+// Sites without a favicon.ico or with a favicon at another path:
+// viber.com, reuters.com, rsf.org, wikimedia.org, wikileaks.org, dw.com,
+// dailymotion.com
+// Sites that are down long-term: thepiratebay.se
